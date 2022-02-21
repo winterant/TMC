ab4b849bb5383e0e1330c47b393cf98dc5943985
Winterant <winterant@163.com>
2022-02-21 22:48:11 +0800

    update data.py and dataset

--- data.py
+++ data.py
@@ -1,50 +1,52 @@
 import pickle
+import scipy.io
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from torch.utils.data import Dataset
 
-"""
-You can also download dataset "scene15-gist-hog-lbp.pkl" from "百度网盘":
-链接：https://pan.baidu.com/s/1oj8mK5EIencgImd4YmUDTg
-提取码：agy7
-"""
+
+def load_toy_example(train=True):
+    x, y = pickle.load(open('dataset/ToyExample_2views.pkl', 'rb'))
+    num_train = int(len(y) * 0.8)
+    if train:
+        for v, k in enumerate(x.keys()):
+            x[v] = x[k][:num_train]
+        y = y[:num_train]
+    else:
+        for v, k in enumerate(x.keys()):
+            x[v] = x[k][num_train:]
+        y = y[num_train:]
+    for k in x.keys():
+        x[k] = MinMaxScaler([0, 1]).fit_transform(x[k]).astype(np.float32)
+    return x, y.astype(np.int64)
 
 
 class MultiViewDataset(Dataset):
-    def __init__(self, data_path='dataset/handwritten_6views.pkl', train=True, train_rate=0.8, shuffle=False):
+    def __init__(self, data_path='dataset/handwritten_6views.mat', train=True):
         super().__init__()
-
-        # Load dataset and shuffle the samples
-        x, y = pickle.load(open(data_path, 'rb'))
-        if shuffle:
-            np.random.seed(97)  # Ensure `rand_indices` is always same.
-            rand_indices = np.random.permutation(len(y))
-            for i in x.keys():
-                x[i] = x[i][rand_indices]
-            y = y[rand_indices]
-
-        # Normalization.
-        for i in x.keys():
-            x[i] = MinMaxScaler([0, 1]).fit_transform(x[i]).astype(np.float32)
-        y = np.array(y, dtype=np.int64).flatten()
-
-        # Split train and test
-        num_train = int(len(y) * train_rate)
+        if data_path == 'toy-example':
+            self.x, self.y = load_toy_example(train)
+            return
+        dataset = scipy.io.loadmat(data_path)
+        mode = 'train' if train else 'test'
+        num_views = int((len(dataset) - 5) / 2)
         self.x = dict()
-        if train:
-            for v, k in enumerate(x.keys()):
-                self.x[v] = x[k][:num_train]
-            self.y = y[:num_train]
-        else:
-            for v, k in enumerate(x.keys()):
-                self.x[v] = x[k][num_train:]
-            self.y = y[num_train:]
+        for k in range(num_views):
+            view = dataset[f'x{k+1}_{mode}']
+            self.x[k] = MinMaxScaler([0, 1]).fit_transform(view).astype(np.float32)
+        self.y = dataset[f'gt_{mode}'].flatten()
+        if min(self.y) > 0:
+            self.y -= 1
 
     def __getitem__(self, index):
         x = dict()
         for v in self.x.keys():
             x[v] = self.x[v][index]
-        return x, self.y[index]
+        return {
+            'x': x,
+            'y': self.y[index],
+            'index': index
+        }
 
     def __len__(self):
         return len(self.y)
